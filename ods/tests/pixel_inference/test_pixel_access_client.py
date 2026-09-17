f86f830d0f0ca3dda8cdacea5ee1689c07d40a67
root <root@DESKTOP-1A6OPC9.localdomain>
2026-09-17 17:30:05 +0000

    test: place pixel contract tests under the pytest suite directory

--- ods/tests/pixel_inference/test_pixel_access_client.py
+++ ods/tests/pixel_inference/test_pixel_access_client.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 import pytest
 
-sys.path.insert(0, str(Path(__file__).resolve().parents[1] / 'bin'))
+sys.path.insert(0, str(Path(__file__).resolve().parents[2] / 'bin'))
 import pixel_access_client as client
 
 pytestmark = pytest.mark.skipif(os.name != 'posix',
