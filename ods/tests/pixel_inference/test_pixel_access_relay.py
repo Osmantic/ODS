20e8d81fac763c5a0c6de8ac9308532698db84d4
root <root@DESKTOP-1A6OPC9.localdomain>
2026-09-17 17:31:45 +0000

    test: place pixel contract tests under the pytest suite directory

--- ods/tests/pixel_inference/test_pixel_access_relay.py
+++ ods/tests/pixel_inference/test_pixel_access_relay.py
@@ -13,7 +13,7 @@ from pathlib import Path
 
 import pytest
 
-sys.path.insert(0, str(Path(__file__).resolve().parents[1] / 'bin'))
+sys.path.insert(0, str(Path(__file__).resolve().parents[2] / 'bin'))
 import pixel_access_relay as relay
 from pixel_access_relay import (
     AccessRelayError, public_model_control, request_runtime_access,
