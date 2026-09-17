ba69a597a1b058beeaaf9c884066d82ea2a060d7
root <root@DESKTOP-1A6OPC9.localdomain>
2026-09-17 17:30:58 +0000

    test: place pixel contract tests under the pytest suite directory

--- ods/tests/pixel_inference/test_pixel_model_contract.py
+++ ods/tests/pixel_inference/test_pixel_model_contract.py
@@ -13,7 +13,7 @@ from pathlib import Path
 
 import pytest
 
-sys.path.insert(0, str(Path(__file__).resolve().parents[1] / 'bin'))
+sys.path.insert(0, str(Path(__file__).resolve().parents[2] / 'bin'))
 from pixel_model_contract import ModelError, binding, checksum, plan, projection, target
 
 
